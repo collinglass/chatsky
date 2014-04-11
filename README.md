6df00b2d1e2665b121cc7e23effb8e34d1a8bb98
Collin Glass <collinglass@gmx.com>
2014-04-10 22:58:12 -0400

    All of it

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chatsky [chat-ski]
+
+Playing with websockets. Simple chat app.
+
+
+### frontend
+
+```index.html```
+ Contains all the goodies. Inline javascript angular app.
+
+### backend
+
+```server.go```
+Gorilla websockets pkg.
---
+++ server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var connections map[*websocket.Conn]bool
+
+func sendAll(msg []byte) {
+	for conn := range connections {
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			delete(connections, conn)
+			return
+		}
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	connections[conn] = true
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			delete(connections, conn)
+			return
+		}
+		log.Println(string(msg))
+		sendAll(msg)
+	}
+}
+
+func main() {
+	connections = make(map[*websocket.Conn]bool)
+	// command line flags
+	port := flag.Int("port", 8080, "port to serve on")
+	dir := flag.String("directory", "app/", "directory of app files")
+	flag.Parse()
+
+	// handle all requests by serving a file of the same name
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+	http.Handle("/", fileHandler)
+	http.HandleFunc("/ws", wsHandler)
+
+	log.Printf("Running on port %d\n", *port)
+
+	addr := fmt.Sprintf("127.0.0.1:%d", *port)
+
+	// this call blocks -- the profram runs here forever
+	err := http.ListenAndServe(addr, nil)
+	fmt.Println(err.Error())
+}
